20abbb222a800031ac5e7a6e03590d9daa51d94d
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-04-01 19:44:51 -0400

    exercise 1 complete

---
+++ answers/exercise1.sql
@@ -0,0 +1,8 @@
+mysql> SELECT * FROM Enrolments LEFT JOIN Students on Enrolments.Student_id = Students.Student_id;
++------------+--------+-------------+----------------+--------------+------------+---------+------------+
+| Student_id | Course | StudentName | Address        | City         | PostalCode | Country | Student_id |
++------------+--------+-------------+----------------+--------------+------------+---------+------------+
+|          2 | Java   | Bob Jones   | 123 east st    | Philadelphia | 19148      | USA     |          2 |
+|          3 | Python | Mark Adams  | 1256 S 13th St | Philadelphia | 19146      | USA     |          3 |
++------------+--------+-------------+----------------+--------------+------------+---------+------------+
+2 rows in set (0.00 sec)
